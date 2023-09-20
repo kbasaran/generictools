f66400cf1abce3410b38e1fdc7bcf9784ba82fd6
kbasaran <kbasaran@gmail.com>
2023-09-20 08:46:37 +0200

    remove GPL license notes

--- signal_tools.py
+++ signal_tools.py
@@ -1,20 +1,3 @@
-# This file is part of Linecraft - Frequency response display and statistics tool
-# Copyright (C) 2023 - Kerem Basaran
-# kbasaran@gmail.com - https://github.com/kbasaran
-
-# Linecraft is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3
-# of the License, or (at your option) any later version.
-
-# Linecraft is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public
-# License along with Linecraft. If not, see <https://www.gnu.org/licenses/>
-
 import os
 import numpy as np
 import acoustics as ac  # https://github.com/timmahrt/pyAcoustics
