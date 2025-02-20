98b10018130a67e4e16a39429777e2dcc5d6f669
kbasaran <kbasaran@proton.me>
2025-02-20 18:02:29 +0100

    test multiple values

--- wavelet spectrogram.py
+++ wavelet spectrogram.py
@@ -27,53 +27,55 @@ def parse_curve_copied_from_Klippel(klippel_curve_string):
     return arr
 
 
+# ---- get curve
 with open("test_klippel_mic_signal.txt", "r") as file:
     curve_copied_from_Klippel = file.read()
 
 # curve_copied_from_Klippel = pyperclip.paste()
 
+# ---- parse
 xy = parse_curve_copied_from_Klippel(curve_copied_from_Klippel)
-x = xy[:, 0] / 1000
+x = xy[:, 0] / 1000  # change ms to seconds
 y = xy[:, 1]
 
 BW = 1/24
-B = 36
-C = 0.5
+# B = 4
+# C = 1
 
-
-# wavelets = [f"cmor{b:.1f}-{c:.1f}"
-#             for b in [B/4, B/2, B, B*2, B*4]
-#             for c in [C/16, C/8, C/4, C/2, C, C*2, C*4]
-#             ]
+wavelets = [f"cmor{b:.1f}-{c:.1f}"
+            for b in [2, 4, 8, 16]
+            for c in [0.5, 1, 1.5, 2]
+            ]
 results = dict()
-# for wavelet in wavelets:
+for wavelet in wavelets:
 
-wavelet = f"cmor{B:.1f}-{C:.1f}"
-sampling_period = np.median(np.diff(x))  # seconds
-FS = int(round(1 / sampling_period))
-
-frequencies = signal_tools.generate_log_spaced_freq_list(20, 10000, 1/BW)
-# normalized to sampling freq. 1 is nyquist.
-scales = pywt.frequency2scale(wavelet, frequencies / FS)
-
-start_time = time.perf_counter()
-cwtmatr, freqs = pywt.cwt(
-    y, scales, wavelet, sampling_period=sampling_period)
-# absolute take absolute value of complex result
-cwtmatr = np.abs(cwtmatr[:-1, :-1])
-print(f"{wavelet} calculated in"
-      f"{(time.perf_counter() - start_time) / 60:.2g} minutes."
-      )
-results[wavelet] = cwtmatr
-
-# plot result using matplotlib's pcolormesh (image with annoted axes)
-fig, ax = plt.subplots()
-pcm = ax.pcolormesh(x, freqs, cwtmatr, edgecolors=None)
-plt.minorticks_on()
-plt.grid(which="minor", axis="both", color="w", alpha=0.07)
-ax.set_yscale("log")
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Frequency (Hz)")
-ax.set_title(f"Continuous Wavelet Transform (Scaleogram)\n{wavelet}")
-fig.colorbar(pcm, ax=ax)
-plt.show()
+    # ---- wavelet calculation
+    # wavelet = f"cmor{B:.1f}-{C:.1f}"
+    sampling_period = np.median(np.diff(x))  # seconds
+    FS = int(round(1 / sampling_period))
+    
+    frequencies = signal_tools.generate_log_spaced_freq_list(20, 10000, 1/BW)
+    # normalized to sampling freq. 1 is nyquist.
+    scales = pywt.frequency2scale(wavelet, frequencies / FS)
+    
+    start_time = time.perf_counter()
+    cwtmatr, freqs = pywt.cwt(
+        y, scales, wavelet, sampling_period=sampling_period)
+    # absolute take absolute value of complex result
+    cwtmatr = np.abs(cwtmatr[:-1, :-1])
+    print(f"{wavelet} calculated in"
+          f"{ (time.perf_counter() - start_time) / 60:.2g} minutes."
+          )
+    results[wavelet] = cwtmatr
+    
+    # ---- plot result using matplotlib's pcolormesh (image with annoted axes)
+    fig, ax = plt.subplots()
+    pcm = ax.pcolormesh(x, freqs, cwtmatr, edgecolors=None)
+    plt.minorticks_on()
+    plt.grid(which="minor", axis="both", color="w", alpha=0.07)
+    ax.set_yscale("log")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Frequency (Hz)")
+    ax.set_title(f"Continuous Wavelet Transform (Scaleogram)\n{wavelet}")
+    fig.colorbar(pcm, ax=ax)
+    plt.show()
