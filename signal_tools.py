a3d9a851551199fe4da57b02d9106de60d41c2c6
kbasaran <kbasaran@proton.me>
2025-08-02 21:27:50 +0200

    version updates

--- signal_tools.py
+++ signal_tools.py
@@ -1,3 +1,21 @@
+# This file is part of Test Signal Maker - Loudspeaker testing tool
+# Copyright (C) 2026 - Kerem Basaran
+# https://github.com/kbasaran
+__email__ = "kbasaran@gmail.com"
+
+# Linecraft is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+
+# Linecraft is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public
+# License along with Linecraft. If not, see <https://www.gnu.org/licenses/>
+
 import os
 import numpy as np
 import pandas as pd
@@ -9,7 +27,6 @@ from scipy import signal as sig
 from functools import lru_cache
 import time
 import multiprocessing
-
 
 import logging
 if __name__ == "__main__":
